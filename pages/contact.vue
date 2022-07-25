05d6250634729c1268eeee9d1381903640a9b594
tm-ox <timothy@oxenfree.net>
2022-07-25 09:38:18 +0700

    Fix Form Margins

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,12 +5,14 @@
       <LogoStacked class="page-logo" />
       <!-- </FadeDown>
       <FadeUp> -->
-      <iframe
-        src="https://www.cognitoforms.com/f/6Ax0zmUI6UeB6_7JUBHcjA/18"
-        style="border: 0; width: 400px"
-        height="513"
-        class="flex justify-center w-full"
-      ></iframe>
+      <div>
+        <iframe
+          src="https://www.cognitoforms.com/f/6Ax0zmUI6UeB6_7JUBHcjA/18"
+          style="border: 0; width: 400px"
+          height="513"
+          class="flex justify-center w-full mx-auto"
+        ></iframe>
+      </div>
       <!-- </FadeUp> -->
     </div>
   </section>
