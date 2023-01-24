bcc083d3c97579c38d2035c28f2ba586c48ef482
TM <tm@oxenfree.net>
2023-01-24 10:41:44 +0700

    privacy policy

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -3,7 +3,6 @@
     <section class="min-h-screen py-24 sm:py-36">
       <FadeUp class="container">
         <ContentDoc path="/privacy" v-slot="{ doc }">
-          <h1>Privacy</h1>
           <h1 class="text-left mb-6">{{ doc.header }}</h1>
           <div class="gap-6">
             <ContentRenderer :value="doc" class="text-left" />
