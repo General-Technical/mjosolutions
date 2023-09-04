ca1dc9db711e73aeb5039d569d4d39e77bc1340a
tm-ox <tm@oxenfree.net>
2023-09-04 09:22:48 +0700

    swap broker image to right

--- pages/your-broker.vue
+++ pages/your-broker.vue
@@ -11,14 +11,14 @@
               {{ doc.header }}
             </h1>
             <div class="flex flex-col gap-6 md:flex-row card bg-background/80">
+              <ContentRenderer
+                :value="doc"
+                class="w-full gap-4 text-left md:w-2/3 lg:columns-2"
+              />
               <div
                 bgImage
                 :style="{ backgroundImage: 'url(' + doc.image + ')' }"
                 class="z-50 flex w-full bg-center bg-cover h-96 md:w-1/3 rounded-mjo"
-              />
-              <ContentRenderer
-                :value="doc"
-                class="w-full gap-4 text-left md:w-2/3 lg:columns-2"
               />
             </div>
           </div>
